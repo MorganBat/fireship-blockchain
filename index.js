999ee9977f3d55aac5fb2770b136da4a5b712237
Morgan Batterham <morgan@morganbatterham.me>
2021-03-08 17:46:01 +1100

    create Wallet class in index.ts

--- index.js
+++ index.js
@@ -56,3 +56,21 @@ class Chain {
     }
 }
 Chain.instance = new Chain();
+class Wallet {
+    constructor() {
+        const keypair = crypto.generateKeyPairSync('rsa', {
+            modulusLength: 2048,
+            publicKeyEncoding: { type: 'spki', format: 'pem' },
+            privateKeyEncoding: { type: 'pkcs8', format: 'pem' },
+        });
+        this.privateKey = keypair.privateKey;
+        this.publicKey = keypair.publicKey;
+    }
+    sendMoney(amount, payeePublicKey) {
+        const transaction = new Transaction(amount, this.publicKey, payeePublicKey);
+        const sign = crypto.createSign('SHA256');
+        sign.update(transaction.toString()).end();
+        const signature = sign.sign(this.privateKey);
+        Chain.instance.addBlock(transaction, this.publicKey, signature);
+    }
+}
